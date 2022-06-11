79427fc9915b7a0cd815851271d6342715f7f855
Daniel López <daniel.lp1094@gmail.com>
2022-06-12 01:13:03 +0200

    FEAT: add test runner for the whole web

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import './App.css';
 export default function App() {
   return (
     <>
-      <h1 className="App">Todo Limpio</h1>
+      <div data-testid="App" className='App'>
+        <p>Todo Limpio</p>
+      </div>
     </>
   );
 }
