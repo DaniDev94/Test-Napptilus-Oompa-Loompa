f60cb3ba3e62695b1ad65b4e6ab95bdca8760dd1
Daniel López <daniel.lp1094@gmail.com>
2022-06-12 13:39:54 +0200

    REFACTOR: remove input component from root file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Header from "./components/Header/Header";
-import FilterInput from "./components/FilterInput/FilterInput";
 import "./App.css";
 
 export default function App() {
   return (
     <div data-testid="App" className="App">
       <Header></Header>
-      <FilterInput></FilterInput>
     </div>
   );
 }
