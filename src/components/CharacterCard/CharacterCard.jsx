9c52d1d2317f8a93aac80069c4942af0d4bc7ee9
Daniel López <daniel.lp1094@gmail.com>
2022-06-12 21:35:24 +0200

    REFACTOR:

--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import willy from "../../assets/images/willy.jpg";
 import "./CharacterCard.scss";
 
-export default function CharacterCard(props) {
+export default function CharacterCard({image, title, gender, job}) {
   return (
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-lg-4 col-md-6 col-sm-10 my-4">
           <div className="card border-0 b-content-card">
-            <img className="card-img-top b-content-card__img" src={props.image ? props.image : willy} alt="not-found" />
+            <img className="card-img-top b-content-card__img" src={image ? image : willy} alt="not-found" />
             <div className="card-body text-md-start">
-              <h3 className="card-title hover-title mt-2">{props.title}</h3>
+              <h3 className="card-title hover-title mt-2">{title}</h3>
               <div className="card-text">
-                <p className="m-0 custom-txt">{props.gender}</p>
-                <p className="m-0 custom-txt">{props.job}</p>
+                <p className="m-0 custom-txt">{gender}</p>
+                <p className="m-0 custom-txt">{job}</p>
               </div>
             </div>
           </div>
