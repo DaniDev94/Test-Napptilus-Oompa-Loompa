163997620a46c3cd7ebde35989d991ec200f9c03
Daniel López <daniel.lp1094@gmail.com>
2022-06-12 22:10:07 +0200

    FEAT: add react-router-dom, implement paths for website and add new info in readme

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.scss";
 import Oompa from "../../assets/images/icons/oompa-icon.png";
 
 export default function Header() {
   return (
     <div className="b-header d-flex align-items-center">
-      <img className="b-header__img" src={Oompa} alt="oompa-icon" />
+      <Link to="/">
+        <img className="b-header__img" src={Oompa} alt="oompa-icon" />
+      </Link>
       <p className="m-0 b-header__txt text-nowrap">Oompa Loompa's crew</p>
     </div>
   );
